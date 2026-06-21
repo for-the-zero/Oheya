a40dcb32f44c944219eb3025c070315e95170e69
for-the-zero <forthezero@outlook.com>
2026-06-21 16:36:14 +0800

    fixed bug

--- src/app/App.vue
+++ src/app/App.vue
@@ -5,7 +5,7 @@
     import Button from 'primevue/button';
     import Accordion from 'primevue/accordion';
 
-    import { ref, watch, nextTick, computed, type VNodeRef } from 'vue';
+    import { ref, watch, nextTick, type VNodeRef } from 'vue';
     import { useElementSize } from '@vueuse/core';
 
     import Settings from './Settings.vue';
@@ -43,7 +43,7 @@
         <InputGroup>
             <InputText :placeholder="config.lang === 'zh' ? '输入你想要了解的事物吧~' : 'Type What You Want to Know'" v-model="searchbox" />
             <InputGroupAddon>
-                <Button icon="pi pi-search" :disabled="!searchbox" severity="" />
+                <Button icon="pi pi-search" :disabled="!searchbox || status.isGenerating" severity="" />
             </InputGroupAddon>
         </InputGroup>
         <div v-if="status.isGenerating && !status.isGenResult" ref="genContentDiv" class="max-h-[50vh] max-w-full break-all overflow-y-hidden text-gray-500 scrollbar-none scroll-smooth whitespace-pre-wrap" >
@@ -51,11 +51,10 @@
             <i class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-7xl! text-white" :class="`pi ${status.isToolCalling ? (status.isToolCalling === 'search' ? 'pi-search' : 'pi-eye') : 'pi-lightbulb'}`"></i>
         </div>
 
-        <Accordion class="w-full" v-model="activeTargets">
-            <Targets v-for="target, i in status.resObj?.targets" :id="`target-${i}`" :target="target" />
+        <Accordion class="w-full" v-model:value="activeTargets" multiple>
+            <Targets v-for="target, i in status.resObj?.targets" :id="`target-${i}`" :target="target" :active="activeTargets.includes(`target-${i}`)" />
         </Accordion>
         
-
         <TransitionGroup name="item-float" tag="div" class="w-full flex flex-col justify-center items-center gap-5">
             <ClassicItem v-for="i in status.resObj?.classic" :key="i" :item="status.toolResults[i]" />
         </TransitionGroup>
@@ -63,7 +62,7 @@
     <Toast />
 </template>
 
-<style>
+<style scope>
     .item-float-enter-active {
         transition: all 0.25s cubic-bezier(0.3, 0.35, 0, 1);
     }
--- src/app/targets.vue
+++ src/app/targets.vue
@@ -12,7 +12,7 @@
     import ClassicItem from './classic-item.vue';
     import Chart from './Charts.vue';
 
-    const { target, id } = defineProps<{ target: NonNullable<result['targets']>[number], id: string }>();
+    const { target, id, active } = defineProps<{ target: NonNullable<result['targets']>[number], id: string, active: boolean }>();
     import { ref } from 'vue';
     import { useGlobalRefStore } from './utils/globalRef';
     import { storeToRefs } from 'pinia';
@@ -61,62 +61,87 @@
             </div>
         </AccordionHeader>
         <AccordionContent>
-            <div class="flex flex-col gap-2">
+            <template v-if="active">
+                <div class="flex flex-col gap-2">
 
-                <h1 v-if="target.brief" class="text-xl">{{ config.lang === 'zh' ? '介绍' : 'Introduction' }}</h1>
-                <p v-if="target.brief">{{ target.brief }}</p>
-                <Divider />
-
-                <h1 v-if="target.descriptions && target.descriptions.length > 0" class="text-xl">{{ config.lang === 'zh' ? '描述' : 'Descriptions' }}</h1>
-                <div v-if="target.descriptions && target.descriptions.length > 0">
-                    <span v-for="desc in target.descriptions" class="mr-4 underline underline-offset-3 decoration-2 cursor-pointer" :style="`text-decoration-color: ${getCfdcColor(desc.confidence)}`" @click="(e)=>{openPop(e,{cite: desc.cite, confidence: desc.confidence})}">{{ desc.text }} "</span>
-                </div>
-                <Divider />
-
-                <h1 v-if="target.attributes && target.attributes.length > 0" class="text-xl">{{ config.lang === 'zh' ? '属性' : 'Attr' }}</h1>
-                <div v-if="target.attributes && target.attributes.length > 0">
-                    <span v-for="attr in target.attributes" class="mr-4 **:data-[pc-section='root']:p-2! cursor-pointer" @click="(e)=>{openPop(e,{cite: attr.cite, confidence: attr.confidence})}">
-                        <Fieldset class="inline-block" :pt="{legend: {style: `padding: 2px;border: none;`}, content: {style: `transform: translateY(-5px);color: ${getCfdcColor(attr.confidence)}`}}">
-                            <template #legend>
-                                <span class="text-xs">{{ attr.akey }}</span>
-                            </template>
-                            <span class="text-sm font-bold">{{ attr.avalue }}</span>
-                        </Fieldset>
-                    </span>
-                </div>
-                <Divider />
-
-                <h1 v-if="target.tags && target.tags.length > 0" class="text-xl">{{ config.lang === 'zh' ? '标签' : 'Tags' }}</h1>
-                <div v-if="target.tags && target.tags.length > 0">
-                    <div v-for="tag in target.tags" class="inline mr-4 cursor-pointer" @click="(e)=>{openPop(e,{cite: tag.cite, confidence: tag.confidence})}">
-                        <Chip>
-                            <div class="flex flex-row items-center justify-center gap-2">
-                                <span class="rounded-full pi pi-tag p-2" :style="`background-color: ${getCfdcColor(tag.confidence)}`" ></span>
-                                <span class="font-medium" >{{ tag.tag }}</span>
-                            </div>
-                        </Chip>
+                <Transition name="float-up" appear>
+                    <div v-if="target.brief" class="flex flex-col gap-2">
+                        <h1 class="text-xl">{{ config.lang === 'zh' ? '介绍' : 'Introduction' }}</h1>
+                        <p>{{ target.brief }}</p>
                     </div>
-                </div>
-                <Divider />
+                </Transition>
+                <Divider v-if="target.brief" />
 
-                <h1 v-if="target.charts && target.charts.length > 0" class="text-xl">{{ config.lang === 'zh' ? '图表' : 'Charts' }}</h1>
-                <Chart v-if="target.charts && target.charts.length > 0" v-for="chart in target.charts" :chart="chart" />
-                <Divider />
-
-                <h1 v-if="target.related && target.related.length > 0" class="text-xl">{{ config.lang === 'zh' ? '相关内容' : 'Related' }}</h1>
-                <div v-if="target.related && target.related.length > 0">
-                    <div v-for="rel in target.related" class="inline mr-4 cursor-pointer" @click="(e)=>{openPop(e,{cite: rel.cite, confidence: rel.confidence})}">
-                        <Chip>
-                            <div class="flex flex-row items-center justify-center gap-2">
-                                <span class="rounded-full pi pi-link p-2" :style="`background-color: ${getCfdcColor(rel.confidence)}`" ></span>
-                                <span class="font-medium" >{{ rel.name }}</span>
-                            </div>
-                        </Chip>
+                <Transition name="slide-right" appear>
+                    <div v-if="target.descriptions && target.descriptions.length > 0" class="flex flex-col gap-2">
+                        <h1 class="text-xl">{{ config.lang === 'zh' ? '描述' : 'Descriptions' }}</h1>
+                        <div>
+                            <span v-for="desc in target.descriptions" class="mr-4 underline underline-offset-3 decoration-2 cursor-pointer" :style="`text-decoration-color: ${getCfdcColor(desc.confidence)}`" @click="(e)=>{openPop(e,{cite: desc.cite, confidence: desc.confidence})}">" {{ desc.text }} "</span>
+                        </div>
                     </div>
-                </div>
+                </Transition>
+                <Divider v-if="target.descriptions && target.descriptions.length > 0" />
+
+                <Transition name="slide-right" appear>
+                    <div v-if="target.attributes && target.attributes.length > 0" class="flex flex-col gap-2">
+                        <h1 class="text-xl">{{ config.lang === 'zh' ? '属性' : 'Attr' }}</h1>
+                        <div>
+                            <span v-for="attr in target.attributes" class="mr-4 **:data-[pc-section='root']:p-2! cursor-pointer" @click="(e)=>{openPop(e,{cite: attr.cite, confidence: attr.confidence})}">
+                                <Fieldset class="inline-block" :pt="{legend: {style: `padding: 2px;border: none;`}, content: {style: `transform: translateY(-5px);color: ${getCfdcColor(attr.confidence)}`}}">
+                                    <template #legend>
+                                        <span class="text-xs">{{ attr.akey }}</span>
+                                    </template>
+                                    <span class="text-sm font-bold">{{ attr.avalue }}</span>
+                                </Fieldset>
+                            </span>
+                        </div>
+                    </div>
+                </Transition>
+                <Divider v-if="target.attributes && target.attributes.length > 0" />
+
+                <Transition name="slide-right" appear>
+                    <div v-if="target.tags && target.tags.length > 0" class="flex flex-col gap-2">
+                        <h1 class="text-xl">{{ config.lang === 'zh' ? '标签' : 'Tags' }}</h1>
+                        <div>
+                            <div v-for="tag in target.tags" class="inline mr-4 cursor-pointer" @click="(e)=>{openPop(e,{cite: tag.cite, confidence: tag.confidence})}">
+                                <Chip>
+                                    <div class="flex flex-row items-center justify-center gap-2">
+                                        <span class="rounded-full pi pi-tag p-2" :style="`background-color: ${getCfdcColor(tag.confidence)}`" ></span>
+                                        <span class="font-medium" >{{ tag.tag }}</span>
+                                    </div>
+                                </Chip>
+                            </div>
+                        </div>
+                    </div>
+                </Transition>
+                <Divider v-if="target.tags && target.tags.length > 0" />
+
+                <Transition name="float-up" appear>
+                    <div v-if="target.charts && target.charts.length > 0" class="flex flex-col gap-2">
+                        <h1 class="text-xl">{{ config.lang === 'zh' ? '图表' : 'Charts' }}</h1>
+                        <Chart v-for="chart in target.charts" :chart="chart" />
+                    </div>
+                </Transition>
+                <Divider v-if="target.charts && target.charts.length > 0" />
+
+                <Transition name="slide-right" appear>
+                    <div v-if="target.related && target.related.length > 0" class="flex flex-col gap-2">
+                        <h1 class="text-xl">{{ config.lang === 'zh' ? '相关内容' : 'Related' }}</h1>
+                        <div>
+                            <div v-for="rel in target.related" class="inline mr-4 cursor-pointer" @click="(e)=>{openPop(e,{cite: rel.cite, confidence: rel.confidence})}">
+                                <Chip>
+                                    <div class="flex flex-row items-center justify-center gap-2">
+                                        <span class="rounded-full pi pi-link p-2" :style="`background-color: ${getCfdcColor(rel.confidence)}`" ></span>
+                                        <span class="font-medium" >{{ rel.name }}</span>
+                                    </div>
+                                </Chip>
+                            </div>
+                        </div>
+                    </div>
+                </Transition>
 
             </div>
-
+            </template>
             <Popover ref="pop">
                 <div class="w-60 flex flex-col gap-3 overflow-y-auto max-h-[50vh]">
                     <ProgressBar v-if="typeof active_pop.confidence === 'number'" class="w-full h-5 shrink-0" :value="active_pop.confidence">{{ config.lang === 'zh' ? '可信度：' : 'Confidence:' }}{{ active_pop.confidence }}</ProgressBar>
@@ -126,3 +151,18 @@
         </AccordionContent>
     </AccordionPanel>
 </template>
+
+<style scoped>
+    .float-up-enter-active, .slide-right-enter-active {
+        transition: all 0.25s cubic-bezier(0.3, 0.35, 0, 1);
+        transition-delay: 0.15s;
+    }
+    .float-up-enter-from {
+        opacity: 0;
+        transform: translateY(0.5rem);
+    }
+    .slide-right-enter-from {
+        opacity: 0;
+        transform: translateX(1rem);
+    }
+</style>
